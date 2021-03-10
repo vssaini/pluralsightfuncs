56ba9c1e6b21e242e9fd792a8a025f7f4b4c0740
vssaini <vs00saini@gmail.com>
2021-03-10 21:21:51 +0530

    Add project files.

---
+++ OnPaymentReceived.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace pluralsightfuncs
+{
+    public static class OnPaymentReceived
+    {
+        [FunctionName("OnPaymentReceived")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, 
+            [Queue("orders")] IAsyncCollector<Order> orderQueue,
+            ILogger log)
+        {
+            log.LogInformation("Received a payment.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+
+            await orderQueue.AddAsync(order); // Adds the order to queue
+
+            log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
+
+            return new OkObjectResult("Thank you for your purchase");
+        }
+    }
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string Email { get; set; }
+        public decimal Price { get; set; }
+    }
+}

6491848e47a01ec9791a10cdbccf8dee2afa7fd9
vssaini <vs00saini@gmail.com>
2021-03-12 21:56:50 +0530

    Output binding to Table storage

--- OnPaymentReceived.cs
+++ OnPaymentReceived.cs
@@ -15,14 +15,18 @@ namespace pluralsightfuncs
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             [Queue("orders")] IAsyncCollector<Order> orderQueue,
+            [Table("orders")] IAsyncCollector<Order> orderTable,
             ILogger log)
         {
             log.LogInformation("Received a payment.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var order = JsonConvert.DeserializeObject<Order>(requestBody);
-
             await orderQueue.AddAsync(order); // Adds the order to queue
+
+            order.PartitionKey = "orders"; // just one partition
+            order.RowKey = order.OrderId;
+            await orderTable.AddAsync(order);
 
             log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
 
@@ -32,6 +36,10 @@ namespace pluralsightfuncs
 
     public class Order
     {
+        // For table storage
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        
         public string OrderId { get; set; }
         public string ProductId { get; set; }
         public string Email { get; set; }
